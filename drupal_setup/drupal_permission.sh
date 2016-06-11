b54c7423aa26c3959a4a09a7d3ddf4ab11a1053f
basivireddy <d.basivireddy@gmail.com>
2016-06-11 21:12:36 +0530

    drupal_setup

---
+++ drupal_setup/drupal_permission.sh
@@ -0,0 +1,5 @@
+sudo mv /var/www/html/drupal-7.25 /var/www/html/drupal
+sudo mkdir /var/www/html/drupal/sites/default/files
+sudo chown www-data:www-data /var/www/html/drupal/sites/default/files
+sudo cp /var/www/html/drupal/sites/default/default.settings.php /var/www/html/drupal/sites/default/settings.php
+sudo chown www-data:www-data /var/www/html/drupal/sites/default/settings.php
